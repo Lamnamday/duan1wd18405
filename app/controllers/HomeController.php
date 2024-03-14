8207c92b3b5fcf55c01efc739732b362d075cbf5
Lamnamday <163096151+Lamnamday@users.noreply.github.com>
2024-03-14 12:45:55 +0700

    Add files via upload

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,80 @@
+<?php
+    require_once MODELS_URL . "LoaiModel.php";
+    require_once MODELS_URL . "ProductsModel.php";
+    require_once MODELS_URL . "DanhMucModel.php";
+    require_once MODELS_URL . "BinhLuanModel.php";
+
+    // TRANG CHU
+    function home() {
+        $products = getShowAllProducts();
+        include VIEWS_URL . "home/index.php";
+    }
+
+    // TRANG LOC
+    function loc() {
+        $maLoai = $_GET["maLoai"];
+        $loai = getLoaiID($maLoai);
+        
+        if(getDanhMucMaLoai($maLoai)) {
+            $listDanhMuc = getDanhMucMaLoai($maLoai);
+
+            $maDanhMuc = $listDanhMuc[0]["maDanhMuc"];
+
+            if(isset($_GET["maDanhMuc"])) {
+                $maDanhMuc = $_GET["maDanhMuc"];
+            }
+
+            $products = getShowProductsDanhMuc($maDanhMuc);
+
+            $filter = 1;
+            if($_SERVER["REQUEST_METHOD"] == "POST") {
+                $filter = $_POST["filter-type"];
+
+                switch($filter) {
+                    case 1:
+                    $products = getShowProductsDanhMuc($maDanhMuc);
+                    break;
+                    case 2:
+                    $products = getShowProductsPriceAscDanhMuc($maDanhMuc);
+                    break;
+                    case 3:
+                    $products = getShowProductsPriceDescDanhMuc($maDanhMuc);
+                    break;
+                    case 4:
+                    $products = getShowProductsBestSellDanhMuc($maDanhMuc);
+                    break;
+                }
+            }
+        }
+
+        include VIEWS_URL . "home/loc.php";
+    }
+
+    // TRANG TIM KIEM SAN PHAM
+    function search() {
+        if(isset($_GET["keyword"])) {
+            $keyWord = $_GET["keyword"];
+            $products = getProductKeyword($keyWord);
+            include VIEWS_URL . "home/search.php";
+        }
+    }
+
+    // TRANG CHI TIET SAN PHAM
+    function chitietsp() {
+        $maSanPham = $_GET["maSanPham"];
+        
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            $comment = $_POST["comment"];
+            insertCommentProduct($_COOKIE["maUser"],$comment,$maSanPham);
+        }
+
+        if(isset($_COOKIE["maUser"])) {
+            $user = getUserID($_COOKIE["maUser"]);
+        }
+        
+        $sanPham = getProductID($maSanPham);
+        $listBinhLuan = getAllCommentsProduct($maSanPham);
+        include VIEWS_URL . "home/chitietsp.php";
+    }
+
+?>
---
+++ app/views/user/giohang.php
@@ -0,0 +1,7 @@
+<!-- BODY -->
+<div class="body">
+    <div class="grid wide">
+        <h1>Gio hang cua toi</h1>
+        
+    </div>
+</div>
